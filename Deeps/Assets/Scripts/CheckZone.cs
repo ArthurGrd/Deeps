4b95ed3272029de4e6d8d473171d611a6814e474
Arthur Garraud <arthur.garraud@epita.fr>
2023-01-19 23:59:26 +0100

    a

--- Deeps/Assets/Scripts/CheckZone.cs
+++ Deeps/Assets/Scripts/CheckZone.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class CheckZone : MonoBehaviour
 {
     public ProgressBar progressBar;
+    public GameObject door;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (transform.name == "Door1Opening" && collision.CompareTag("Player"))
         {
-            progressBar.SetProgress((int)progressBar.slider.value+1);
+            door.SetActive(false);
+            progressBar.SetProgress(1);
+        }
+        else if (transform.name == "Door1Closing" && collision.CompareTag("Player"))
+        {
+            door.SetActive(true);
         }
     }
 }
