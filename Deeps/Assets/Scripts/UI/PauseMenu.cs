06be2e33d486c01dcb018fc01c5bb8c56a03c6bf
Arthur Garraud <arthur.garraud@epita.fr>
2023-03-08 20:54:26 +0100

    V2.0

--- Deeps/Assets/Scripts/UI/PauseMenu.cs
+++ Deeps/Assets/Scripts/UI/PauseMenu.cs
@@ -1,9 +1,19 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
-    
+    public GameObject settingsMenuUI;
+    public Slider volumeSlider;
+    public TMP_InputField width;
+    public TMP_InputField height;
+
     private static bool _isGamePaused;
     
     public bool GetIsGamePaused() {return _isGamePaused;}
@@ -11,6 +21,7 @@ public class PauseMenu : MonoBehaviour
     private void Start()
     {
         Resume();
+        settingsMenuUI.SetActive(false);
     }
 
     public void UpdatePauseMenu()
@@ -27,8 +38,23 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
-    
-    
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void OpenSettingsMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(true);
+    }
+
+    public void BackSettingsMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        settingsMenuUI.SetActive(false);
+    }
 
     private void Pause()
     {
@@ -42,5 +68,27 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1;
         _isGamePaused = false;
+    }
+
+    public void UpdateVolume()
+    {
+        float newVolume = volumeSlider.value/10;
+        foreach (Object a in GameObject.FindObjectsOfType(typeof(AudioSource)))
+        {
+            a.GetComponent<AudioSource>().volume = newVolume;
+        }
+    }
+
+    public void ChangeResolution()
+    {
+        int widthint;
+        int heightint;
+        if (int.TryParse(width.text, out widthint))
+        {
+            if (int.TryParse(height.text, out heightint))
+            {
+                Screen.SetResolution(widthint,heightint,Screen.fullScreen);
+            } 
+        }
     }
 }
---
+++ Deeps/Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject startMenuUI;
+
+    private void Start()
+    {
+        foreach (Object obj in GameObject.FindObjectsOfType(typeof(MonoBehaviour)))
+        {
+            GameObject objet;
+            string parentName;
+            if (obj.GameObject().transform.parent != null)
+            {
+                if (obj.GameObject().transform.parent.parent != null)
+                {
+                    if (obj.GameObject().transform.parent.parent.parent != null)
+                    {
+                        objet = obj.GameObject();
+                        parentName = obj.GameObject().transform.parent.parent.parent.name;
+                    }
+                    else
+                    {
+                        objet = obj.GameObject();
+                        parentName = obj.GameObject().transform.parent.parent.name;
+                    }
+                }
+                else
+                {
+                    objet = obj.GameObject();
+                    parentName = obj.GameObject().transform.parent.name;
+                }
+            }
+            else
+            {
+                objet = obj.GameObject();
+                parentName = obj.name;
+            }
+
+            if (parentName!="SpawnScreen")
+            {
+                Destroy(objet);
+            }
+
+        }
+    }
+    
+
+    public void StartSoloGame()
+    {
+        SceneManager.LoadScene("Spawn");
+    }
+
+    public void StartMultiplayerGame()
+    {
+        Debug.Log("a");
+    }
+    
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
